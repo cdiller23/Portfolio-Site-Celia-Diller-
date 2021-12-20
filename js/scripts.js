0a54c331cc7c8ee3c7526c5d8704a1abf034349d
Celia Diller <celiadiller@gmail.com>
2021-12-19 23:08:47 -0500

    Edits made after validators

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-console.log('Hello and welcome to my Portfolio!')
+console.log("Welcome to my portfolio!");
 
 function menuToggle() {
-	var x = document.getElementById('myNavtoggle')
-	if (x.className === 'navtoggle') {
-		x.className += ' responsive'
-	} else {
-		x.className = 'navtoggle'
-	}
-}
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+      } else {
+        x.className = 'navtoggle';
+      }
+    }
